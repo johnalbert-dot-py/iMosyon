1df89fa53a702910c48c61ff9a43f7e499e64b5a
John Albert Flores <floresjohnalbert19@gmail.com>
2022-03-26 04:12:01 +0800

    added core library

--- back-end/core/predictor.py
+++ back-end/core/predictor.py
@@ -1,1 +1,26 @@
 from dataclasses import dataclass
+
+
+@dataclass
+class EmotionPrediction:
+
+    """
+    A Class for a prediction of an emotion.
+    """
+
+    sentence: str
+
+    def expected_emotions(self):
+        """Eight primary emotion base on research"""
+        return [
+            "Anger",
+            "Anticipation",
+            "Joy",
+            "Sadness",
+            "Trust",
+            "Surprise",
+            "Disgust",
+        ]
+
+    def predict(self) -> str:
+        pass
