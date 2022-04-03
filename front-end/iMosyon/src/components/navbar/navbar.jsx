6c95ca72745a7d80c667bd3b6eac61d5c5859392
John Albert Flores <floresjohnalbert19@gmail.com>
2022-04-04 02:05:11 +0800

    style updated

--- front-end/iMosyon/src/components/navbar/navbar.jsx
+++ front-end/iMosyon/src/components/navbar/navbar.jsx
@@ -4,14 +4,36 @@ import { BtnLightCircle, BtnLightCircleOutline, BtnBurger } from '@/components/b
 import './navbar.scss'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowUpRight } from '@fortawesome/pro-solid-svg-icons';
+import { faArrowUpRight, faArrowRight, faArrowUp } from '@fortawesome/pro-solid-svg-icons';
 import { useState, useEffect } from 'react';
+
+import {motion} from 'framer-motion';
 
 
 const Navbar = () => {
+  const arrowStyleDefault = {
+    fontSize: '1.0rem',
+  }
+  const [arrowStyle, setArrowStyle] = useState(arrowStyleDefault);
+  const rotateArrowStraight = () => {
+    setArrowStyle(arrowStyle => {
+      return {
+        ...arrowStyle,
+        transform: 'rotate(45deg)',
+      }
+    });
+  }
+  const rotateArrowUp = () => {
+    setArrowStyle(arrowStyle => {
+      return {
+        ...arrowStyle,
+        transform: 'rotate(0deg)',
+      }
+    });
+  }
 
   return (
-    <div className={"navbar-wrapper " + (isScrolled ? ' bg-dark' : '') + ""}>
+    <div className={"navbar-wrapper"}>
       <nav className='navbar'>
         <div className="navbar-brand">
           <a href="#" className="brand">
@@ -32,10 +54,16 @@ const Navbar = () => {
           <a href="#">
             Sign Up
           </a>
-
-          <BtnLightCircle size="btn-md" width="btn-sm-w" show_on_small="true">
-            Login &nbsp;<FontAwesomeIcon icon={faArrowUpRight} />
-          </BtnLightCircle>
+          <motion.span onHoverStart={rotateArrowStraight} onHoverEnd={rotateArrowUp} animate={{
+            opacity: 1,
+            transition: {
+              duration: 0.5,
+            }
+          }} initial={{opacity: 0}}>
+            <BtnLightCircle size="btn-md" width="btn-sm-w" show_on_small="true">
+            Login &nbsp;<FontAwesomeIcon icon={faArrowUpRight} style={arrowStyle} /> &nbsp;
+            </BtnLightCircle>
+          </motion.span>
 
           <BtnBurger size="btn-md" show_on_small="true" />
         </div>
