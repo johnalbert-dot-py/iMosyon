b94711b411ae6afcea34977ae50639d35e71f926
John Albert Flores <floresjohnalbert19@gmail.com>
2022-04-08 15:22:16 +0800

    added login

---
+++ front-end/iMosyon/src/components/EmojiCircle.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const EmojiCircleWrapper = styled.div`
+width: 65px;
+height: 65px;
+font-size: 30px;
+padding-top: 4px;
+border-radius: 50%;
+background-color: #fff;
+display: flex;
+align-items: center;
+justify-content: center;
+background: ${props => props.background ? props.background : 'red'};
+`
+
+export const EmojiCircle = ({ emoji, background }) => {
+  return (
+    <EmojiCircleWrapper background={ background }>
+      { emoji }
+    </EmojiCircleWrapper>
+  );
+}
+
+export default EmojiCircle;
--- front-end/iMosyon/src/pages/Login/login.jsx
+++ front-end/iMosyon/src/pages/Login/login.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
+import EmojiCircle from '@/components/EmojiCircle';
 
-import './login.css'
+import './login.scss'
 
 export const Login = () => {
   useEffect(() => {
@@ -12,6 +13,32 @@ export const Login = () => {
     <>
       <div className="left">
 
+      </div>
+
+      <div className="contents">
+        <div className="left-content">
+            <div className="circles">
+              <EmojiCircle emoji="😎" background="linear-gradient(136.81deg, #E18B0A 15.77%, #0ABBE1 108.38%)" />
+              <EmojiCircle emoji="😆" background="linear-gradient(54.94deg, #690AE1 13.19%, #FF0EBB 134.86%)" />
+              <EmojiCircle emoji="🤔" background="linear-gradient(70.11deg, #0AE17A 16.53%, #1200DF 109.66%)" />
+            </div>
+            <div className="content">
+              <h1>
+              iMosyon
+              </h1>
+              <p>
+                “Hello World!”
+              </p>
+              <span>
+                -iMosyon Team
+              </span>
+            </div>
+        </div>
+        <div className="right">
+          <h2>
+            Login
+          </h2>
+        </div>
       </div>
     </>
   )
